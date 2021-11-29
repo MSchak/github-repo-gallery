8859f47a76f4f2d66da48201f369ba5dfda68ba9
megan_s <m.k.schacherbauer@gmail.com>
2021-11-29 11:20:32 -0600

    Added back button and search function

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ const overview = document.querySelector(".overview");
 const username = "MSchak";
 const repoList = document.querySelector(".repo-list");
 const repoSection = document.querySelector(".repos");
-const repoData = document.querySelector(".repo-data")
+const repoData = document.querySelector(".repo-data");
+const backButton = document.querySelector(".view-repos");
+const filterInput = document.querySelector(".filter-repos");
 
 
 
@@ -34,13 +36,13 @@ const display = function(user){
 const getRepos = async function() {
   const data = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
   const repos = await data.json();
-  console.log(repos);
     displayInfo(repos)
 };
 
 getRepos();
 
 const displayInfo = function (repos) {
+    filterInput.classList.remove("hide");
   for (const repo of repos){
     const repoListItem = document.createElement("li");
     repoListItem.classList.add("repo");
@@ -84,4 +86,28 @@ const displayRepoInfo = function (repoInfo, languages) {
     repoData.append(repoDataDiv);
     repoData.classList.remove("hide");
     repoSection.classList.add("hide");
+    backButton.classList.remove("hide");
+
 };
+
+backButton.addEventListener("click", function(){
+    repoSection.classList.remove("hide");
+    repoData.classList.add("hide");
+    backButton.classList.add("hide");
+
+});
+
+filterInput.addEventListener("input", function (e) {
+    const input = e.target.value;
+    const reposListItem = document.querySelectorAll(".repo");
+    const searchLower = input.toLowerCase();
+
+    for (const item of reposListItem){
+        const repoLI = item.innerText.toLowerCase();
+        if (repoLI.includes(searchLower)){
+            item.classList.remove("hide");
+        } else {
+            item.classList.add("hide");
+    }
+    }
+});
